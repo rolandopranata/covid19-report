9dc26636e3e8245559c0b94f9215ad6c1a2c5222
RolandoPranataAR <rolandopranata@gmail.com>
2022-06-04 05:05:10 +0700

    fix: restyling ui

--- src/scripts/components/footer-content.js
+++ src/scripts/components/footer-content.js
@@ -1,12 +1,12 @@
-class FooterContent extends HTMLElement{
-    connectedCallback(){
+class FooterContent extends HTMLElement {
+    connectedCallback() {
         this.render();
     }
 
-    render(){
+    render() {
         this.innerHTML = `
         <footer class="container">
-            <p class="footer text-center">&copy; 2021. Dibuat oleh Rolando Pranata.</p>
+            <p class="footer text-center">&copy;2021. dibuat oleh Rolando Pranata.</p>
         </footer>`;
     }
 }
--- src/scripts/view/global.js
+++ src/scripts/view/global.js
@@ -1,21 +1,20 @@
 /* eslint-disable quotes */
 const global = () => {
-  async function getData() {
-    try {
-      const response = await fetch("https://covid19.mathdro.id/api");
-      const responseJson = await response.json();
-      console.log(responseJson);
-      document.querySelector("global-info").coronaItem = responseJson;
-    } catch (error) {
-      showMessage(error);
+    async function getData() {
+        try {
+            const response = await fetch('https://covid19.mathdro.id/api');
+            const responseJson = await response.json();
+            document.querySelector('global-info').coronaItem = responseJson;
+        } catch (error) {
+            showMessage(error);
+        }
     }
-  }
 
-  const showMessage = (message) => {
-    console.log(message);
-  };
+    const showMessage = (message) => {
+        console.log(message);
+    };
 
-  getData();
+    getData();
 };
 
 export default global;
